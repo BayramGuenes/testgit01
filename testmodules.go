c691bc9206b8cd56b59091ec1b646af415b679f9
Bayram <bayram.guenes@gmail.com>
2019-05-28 14:36:18 +0200

    Änderung hotfix 003

--- testmodules.go
+++ testmodules.go
@@ -1,6 +1,7 @@
 package main
 
 func test(iPar string) {
-	println("test-func-  in develop develop branch ")
+	println("test-func-  in hotfix develop branch ")
+	println("test-func-  in hotfix develop branch ")
 	println("test-func-called with " + iPar)
 }
